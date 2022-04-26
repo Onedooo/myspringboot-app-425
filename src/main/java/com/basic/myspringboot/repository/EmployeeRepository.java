da9ffa5727100701e4ddeffa6acd90a3b91a8411
minjae <zuppa812@gmail.com>
2022-04-26 13:38:41 +0900

    new

---
+++ src/main/java/com/basic/myspringboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.basic.myspringboot.repository;
+
+import com.basic.myspringboot.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
--- src/test/java/com/basic/myspringboot/MyspringbootAppApplicationTests.java
+++ src/test/java/com/basic/myspringboot/MyspringbootAppApplicationTests.java
@@ -3,7 +3,6 @@ package com.basic.myspringboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MyspringbootAppApplicationTests {
 
 	@Test
